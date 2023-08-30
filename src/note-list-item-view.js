8c210fd823f8bd5892b75be735858290fef4f921
Takuya Matsuyama <hi@craftz.dog>
2023-08-30 14:45:28 +0900

    fix(context-menu): not working
    do not call stopPropagation
    
    fix #11

--- src/note-list-item-view.js
+++ src/note-list-item-view.js
@@ -58,7 +58,6 @@ export default function SimpleNoteListItemView(props) {
     e => {
       onContextMenu && onContextMenu(e, note)
       e.preventDefault()
-      e.stopPropagation()
     },
     [onContextMenu, note]
   )
